2b2af0f081a01ad1af0cfeb25cc40516831f74d8
lucasceifa <lucassilvatrindade@hotmail.com>
2022-10-06 14:24:03 -0300

    Inventory component and styles

--- src/containers/Inventory/components/InterfaceInventory/InterfaceInventory.jsx
+++ src/containers/Inventory/components/InterfaceInventory/InterfaceInventory.jsx
@@ -2,7 +2,16 @@ import './InterfaceInventory.css'
 
 function InterfaceInventory() {
 	return ( 
-		<div></div>
+		<div className='interfaceContainer'>
+			<div className='leftArm inventoryHoverInterface'></div>
+			<div className='mainBody'>
+				<div className='head inventoryHoverInterface'></div>
+				<div className='bodyInterface inventoryHoverInterface'></div>
+				<div className='legs inventoryHoverInterface'></div>
+				<div className='foot inventoryHoverInterface'></div>
+			</div>
+			<div className='rightArm inventoryHoverInterface'></div>
+		</div>	
 	 );
 }
 
